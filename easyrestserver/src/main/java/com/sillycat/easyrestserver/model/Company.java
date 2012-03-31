ec8a39a966b89eabb80bc5b378f7a99e9fefb13c
luohuazju <luohuazju@gmail.com>
2012-03-31 18:01:20 +0800

    model

---
+++ easyrestserver/src/main/java/com/sillycat/easyrestserver/model/Company.java
@@ -0,0 +1,13 @@
+package com.sillycat.easyrestserver.model;
+
+import java.util.List;
+
+public class Company {
+
+	private Integer id;
+	
+	private String companyName;
+	
+	private List<Person> persons;
+	
+}
---
+++ easyrestserver/src/main/java/com/sillycat/easyrestserver/model/Person.java
@@ -0,0 +1,11 @@
+package com.sillycat.easyrestserver.model;
+
+public class Person {
+	
+	private Integer id;
+
+	private String personName;
+
+	private Company company;
+
+}
