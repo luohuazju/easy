7a736fa33e940b8596897f72fc434d9ff816c290
luohuazju <luohuazju@gmail.com>
2012-04-05 17:43:09 +0800

    3 approach to verify the controller

--- easyrestserver/src/test/java/com/sillycat/easyrestserver/controller/PersonControllerTest1.java
+++ easyrestserver/src/test/java/com/sillycat/easyrestserver/controller/PersonControllerTest1.java
@@ -2,7 +2,6 @@ package com.sillycat.easyrestserver.controller;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 import org.codehaus.jackson.map.ObjectMapper;
@@ -10,44 +9,40 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.sillycat.easyrestserver.core.MockWebApplication;
 import com.sillycat.easyrestserver.core.MockWebApplicationContextLoader;
 import com.sillycat.easyrestserver.model.Company;
 import com.sillycat.easyrestserver.model.Person;
-import com.sillycat.easyrestserver.service.PersonService;
 
+/**
+ * mock the servlet and use servlet to test our controller
+ * servlet.service(mockRequest, mockResponse);
+ * can not mock the service or manager in controller
+ * @author karl
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(loader = MockWebApplicationContextLoader.class)
 @MockWebApplication(name = "easyrestserver", locations = "classpath:test-context.xml")
-public class PersonControllerTest {
+public class PersonControllerTest1 {
 
 	@Autowired
 	private DispatcherServlet servlet;
 
-	@Mock
-	private PersonService mockPersonService;
-
 	ObjectMapper mapper;
-	
-	AnnotationMethodHandlerAdapter handlerAdapter;
-
 	Person person;
-	
-	PersonController personController;
+	MockHttpServletRequest mockRequest;
+	MockHttpServletResponse mockResponse;
 
 	@Before
 	public void setUp() throws ServletException, IOException {
@@ -58,30 +53,22 @@ public class PersonControllerTest {
 		person.setId(1);
 		person.setPersonName("person1");
 		
-		handlerAdapter = new AnnotationMethodHandlerAdapter();
-        HttpMessageConverter<?>[] messageConverters = {new MappingJacksonHttpMessageConverter()};
-        handlerAdapter.setMessageConverters(messageConverters);
-        
-        personController = new PersonController();
-		personController.setPersonService(mockPersonService);
+		mockRequest = new MockHttpServletRequest();
+		mockRequest.setAttribute(HandlerMapping.INTROSPECT_TYPE_LEVEL_MAPPING,true);
 		
+		mockResponse = new MockHttpServletResponse();
 	}
-	
 	
 
 	@Test
 	public void get() throws Exception {
-		MockHttpServletRequest mockRequest = new MockHttpServletRequest("GET",
-				"/person/1");
+		mockRequest.setMethod("GET");
+		mockRequest.setRequestURI("/person/1");
 		mockRequest.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+		mockResponse = new MockHttpServletResponse();
 
-		Mockito.when(mockPersonService.get(1)).thenReturn(person);
-		
 		servlet.service(mockRequest, mockResponse);
 
-		handlerAdapter.handle(mockRequest, mockResponse, personController);
-		Assert.assertEquals(mockResponse.getStatus(), 200);
 		Person actualPerson = mapper.readValue(
 				mockResponse.getContentAsString(), Person.class);
 		Assert.assertEquals(actualPerson.getId(), person.getId());
@@ -89,9 +76,7 @@ public class PersonControllerTest {
 	
 	@Test
 	public void add() throws Exception{
-		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
 		mockRequest.setContentType(MediaType.APPLICATION_JSON_VALUE);
-		MockHttpServletResponse mockResponse = new MockHttpServletResponse();
 		mockRequest.setMethod("POST");
 	    mockRequest.setRequestURI("/person");
 	    
@@ -100,12 +85,11 @@ public class PersonControllerTest {
 
 	    servlet.service(mockRequest, mockResponse);
 	    
-	    handlerAdapter.handle(mockRequest, mockResponse, personController);
-
 	    Assert.assertEquals(mockResponse.getStatus(), 200);
 		Person actualPerson = mapper.readValue(
 				mockResponse.getContentAsString(), Person.class);
 		Assert.assertEquals(actualPerson.getId(), person.getId());
 	}
+
 
 }
