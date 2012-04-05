fcd781bfe4d95bed3fede3ea21986bdc56eaa344
luohuazju <luohuazju@gmail.com>
2012-04-05 18:15:13 +0800

    use spring-test-mvc to test our rest json api

---
+++ easyrestserver/src/test/java/com/sillycat/easyrestserver/controller/PersonControllerTest.java
@@ -0,0 +1,58 @@
+package com.sillycat.easyrestserver.controller;
+
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.server.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.server.request.MockMvcRequestBuilders;
+import org.springframework.test.web.server.setup.MockMvcBuilders;
+
+import com.sillycat.easyrestserver.model.Company;
+import com.sillycat.easyrestserver.model.Person;
+import com.sillycat.easyrestserver.service.PersonService;
+
+public class PersonControllerTest {
+
+	@Mock
+	private PersonService mockPersonService;
+	
+	PersonController personController;
+	
+	Person person;
+	
+	@Before
+	public void setUp() throws ServletException, IOException {
+		MockitoAnnotations.initMocks(this);
+		
+		personController = new PersonController();
+		personController.setPersonService(mockPersonService);
+		
+		person = new Person();
+		person.setCompany(new Company());
+		person.setId(3);
+		person.setPersonName("person3");
+	}
+	
+	@Test
+	public void get() throws Exception {
+		
+		Mockito.when(mockPersonService.get(3)).thenReturn(person);
+		
+		MockMvcBuilders.standaloneSetup(personController).build()
+			.perform(MockMvcRequestBuilders.get("/person/3").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().type(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.personName").value("person3"));
+	}	
+	
+}
