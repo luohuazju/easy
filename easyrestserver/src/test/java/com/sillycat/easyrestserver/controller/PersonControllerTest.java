db74733cdd0bb2ddd842fb5a245e691afa0a53b3
luohuazju <luohuazju@gmail.com>
2012-04-06 14:24:04 +0800

    add post example

--- easyrestserver/src/test/java/com/sillycat/easyrestserver/controller/PersonControllerTest.java
+++ easyrestserver/src/test/java/com/sillycat/easyrestserver/controller/PersonControllerTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -25,34 +26,58 @@ public class PersonControllerTest {
 
 	@Mock
 	private PersonService mockPersonService;
-	
+
 	PersonController personController;
-	
+
 	Person person;
-	
+
+	ObjectMapper jsonMapper;
+
 	@Before
 	public void setUp() throws ServletException, IOException {
 		MockitoAnnotations.initMocks(this);
-		
+
 		personController = new PersonController();
 		personController.setPersonService(mockPersonService);
-		
+
 		person = new Person();
 		person.setCompany(new Company());
 		person.setId(3);
 		person.setPersonName("person3");
+
+		jsonMapper = new ObjectMapper();
 	}
-	
+
 	@Test
 	public void get() throws Exception {
-		
+
 		Mockito.when(mockPersonService.get(3)).thenReturn(person);
-		
-		MockMvcBuilders.standaloneSetup(personController).build()
-			.perform(MockMvcRequestBuilders.get("/person/3").accept(MediaType.APPLICATION_JSON))
+
+		MockMvcBuilders
+				.standaloneSetup(personController)
+				.build()
+				.perform(
+						MockMvcRequestBuilders.get("/person/3").accept(
+								MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
 				.andExpect(content().type(MediaType.APPLICATION_JSON))
 				.andExpect(jsonPath("$.personName").value("person3"));
-	}	
-	
+	}
+
+	@Test
+	public void add() throws Exception {
+		person.setId(null);
+		String jsonPerson = jsonMapper.writeValueAsString(person);
+		MockMvcBuilders
+				.standaloneSetup(personController)
+				.build()
+				.perform(
+						MockMvcRequestBuilders.post("/person")
+								.contentType(MediaType.APPLICATION_JSON)
+								.accept(MediaType.APPLICATION_JSON)
+								.body(jsonPerson.getBytes()))
+				.andExpect(status().isOk())
+				.andExpect(content().type(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.personName").value("person3"));
+	}
 }
