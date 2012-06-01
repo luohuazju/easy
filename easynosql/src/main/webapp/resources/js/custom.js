cc94c4b511b85b5d201b6a0bbe7c2874b33a23cb
luohuazju <luohuazju@gmail.com>
2012-06-01 16:51:40 +0800

    no conflict for velocity and jquery

--- easynosql/src/main/webapp/resources/js/custom.js
+++ easynosql/src/main/webapp/resources/js/custom.js
@@ -1,8 +1,8 @@
 var urlHolder = new Object();
 
 function loadTable() {
-	jQuery.get(urlHolder.records, function(response) {
-		jQuery('#tableUsers').find('tbody').remove();
+	$.get(urlHolder.records, function(response) {
+		$('#tableUsers').find('tbody').remove();
  		for (var i=0; i<response.users.length; i++) {
 			var row = '<tr>';
 			row += '<td><input type="radio" name="index" id="index" value="'+i+'"></td>';
@@ -11,21 +11,21 @@ function loadTable() {
 			row += '<td>' + response.users[i].lastName + '</td>';
 			row += '<td>' + getRole(response.users[i].role.role) + '</td>';
 			row += '</tr>';
-	 		jQuery('#tableUsers').append(row);
+	 		$('#tableUsers').append(row);
  		}
  		
- 		jQuery('#tableUsers').data('model', response.users);
+ 		$('#tableUsers').data('model', response.users);
 		toggleForms('hide'); ;
  	});
 }
 
 function submitNewRecord() {
-	jQuery.post(urlHolder.add, {
-			username: jQuery('#newUsername').val(),
-			password: jQuery('#newPassword').val(),
-			firstName: jQuery('#newFirstName').val(),
-			lastName: jQuery('#newLastName').val(),
-			role: jQuery('#newRole').val()
+	$.post(urlHolder.add, {
+			username: $('#newUsername').val(),
+			password: $('#newPassword').val(),
+			firstName: $('#newFirstName').val(),
+			lastName: $('#newLastName').val(),
+			role: $('#newRole').val()
 		}, 
 		function(response) {
 			if (response != null) {
@@ -41,10 +41,10 @@ function submitNewRecord() {
 }
 
 function submitDeleteRecord() {
-	var selected = jQuery('input:radio[name=index]:checked').val();
+	var selected = $('input:radio[name=index]:checked').val();
 	
-	jQuery.post(urlHolder.del, {
-			username: jQuery('#tableUsers').data('model')[selected].username
+	$.post(urlHolder.del, {
+			username: $('#tableUsers').data('model')[selected].username
 		}, 
 		function(response) {
 			if (response == true) {
@@ -58,11 +58,11 @@ function submitDeleteRecord() {
 }
 
 function submitUpdateRecord() {
-	jQuery.post(urlHolder.edit, {
-			userName: jQuery('#editUsername').val(),
-			firstName: jQuery('#editFirstName').val(),
-			lastName: jQuery('#editLastName').val(),
-			role: jQuery('#editRole').val()
+	$.post(urlHolder.edit, {
+			userName: $('#editUsername').val(),
+			firstName: $('#editFirstName').val(),
+			lastName: $('#editLastName').val(),
+			role: $('#editRole').val()
 		}, 
 		function(response) {
 			if (response != null) {
@@ -88,7 +88,7 @@ function getRole(role) {
 }
 
 function hasSelected() {
-	var selected = jQuery('input:radio[name=index]:checked').val();
+	var selected = $('input:radio[name=index]:checked').val();
 	if (selected == undefined) { 
 		alert('Select a record first!');
 		return false;
@@ -98,55 +98,55 @@ function hasSelected() {
 }
 
 function fillEditForm() {
-	var selected = jQuery('input:radio[name=index]:checked').val();
-	jQuery('#editUsername').val( jQuery('#tableUsers').data('model')[selected].username );
-	jQuery('#editFirstName').val( jQuery('#tableUsers').data('model')[selected].firstName );
-	jQuery('#editLastName').val( jQuery('#tableUsers').data('model')[selected].lastName );
-	jQuery('#editRole').val( jQuery('#tableUsers').data('model')[selected].role.role );
+	var selected = $('input:radio[name=index]:checked').val();
+	$('#editUsername').val( $('#tableUsers').data('model')[selected].username );
+	$('#editFirstName').val( $('#tableUsers').data('model')[selected].firstName );
+	$('#editLastName').val( $('#tableUsers').data('model')[selected].lastName );
+	$('#editRole').val( $('#tableUsers').data('model')[selected].role.role );
 }
 
 function resetNewForm() {
-	jQuery('#newUsername').val('');
-	jQuery('#newPassword').val('');
-	jQuery('#newFirstName').val('');
-	jQuery('#newLastName').val('');
-	jQuery('#newRole').val('2');
+	$('#newUsername').val('');
+	$('#newPassword').val('');
+	$('#newFirstName').val('');
+	$('#newLastName').val('');
+	$('#newRole').val('2');
 }
 
 function resetEditForm() {
-	jQuery('#editFirstName').val('');
-	jQuery('#editLastName').val('');
-	jQuery('#editRole').val('2');
+	$('#editFirstName').val('');
+	$('#editLastName').val('');
+	$('#editRole').val('2');
 }
 
 function toggleForms(id) {
 	if (id == 'hide') {
-		jQuery('#newForm').hide();
-		jQuery('#editForm').hide();
+		$('#newForm').hide();
+		$('#editForm').hide();
 		
 	} else if (id == 'new') {
  		resetNewForm();
- 		jQuery('#newForm').show();
- 		jQuery('#editForm').hide();
+ 		$('#newForm').show();
+ 		$('#editForm').hide();
  		
 	} else if (id == 'edit') {
  		resetEditForm();
- 		jQuery('#newForm').hide();
- 		jQuery('#editForm').show();
+ 		$('#newForm').hide();
+ 		$('#editForm').show();
 	}
 }
 
 function toggleCrudButtons(id) {
 	if (id == 'show') {
-		jQuery('#newBtn').removeAttr('disabled');
-		jQuery('#editBtn').removeAttr('disabled');
-		jQuery('#deleteBtn').removeAttr('disabled');
-		jQuery('#reloadBtn').removeAttr('disabled');
+		$('#newBtn').removeAttr('disabled');
+		$('#editBtn').removeAttr('disabled');
+		$('#deleteBtn').removeAttr('disabled');
+		$('#reloadBtn').removeAttr('disabled');
 		
 	} else if (id == 'hide'){
-		jQuery('#newBtn').attr('disabled', 'disabled');
-		jQuery('#editBtn').attr('disabled', 'disabled');
-		jQuery('#deleteBtn').attr('disabled', 'disabled');
-		jQuery('#reloadBtn').attr('disabled', 'disabled');
+		$('#newBtn').attr('disabled', 'disabled');
+		$('#editBtn').attr('disabled', 'disabled');
+		$('#deleteBtn').attr('disabled', 'disabled');
+		$('#reloadBtn').attr('disabled', 'disabled');
 	}
 }
