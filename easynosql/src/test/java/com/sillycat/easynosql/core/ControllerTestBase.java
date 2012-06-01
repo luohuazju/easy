61823b0dd00590be36de9093722cad8a6e8ca198
luohuazju <luohuazju@gmail.com>
2012-06-01 16:24:39 +0800

    test for groovy controller

---
+++ easynosql/src/test/java/com/sillycat/easynosql/core/ControllerTestBase.java
@@ -0,0 +1,53 @@
+package com.sillycat.easynosql.core;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Before;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.DefaultAnnotationHandlerMapping;
+
+import com.sillycat.easygroovyplugin.servlet.proxy.ProxyAwareAnnotationMethodHandlerAdapter;
+
+public class ControllerTestBase {
+
+	private HandlerMapping handlerMapping;
+	private HandlerAdapter handlerAdapter;
+
+	@Before
+	public void setUp() {
+		String[] configs = { "file:src/test/resources/test-context.xml" };
+		XmlWebApplicationContext context = new XmlWebApplicationContext();
+		context.setConfigLocations(configs);
+		MockServletContext msc = new MockServletContext();
+		context.setServletContext(msc);
+		context.refresh();
+		msc.setAttribute(
+				WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE,
+				context);
+		handlerMapping = (HandlerMapping) context
+				.getBean(DefaultAnnotationHandlerMapping.class);
+		handlerAdapter = (HandlerAdapter) context
+				.getBean(context
+						.getBeanNamesForType(ProxyAwareAnnotationMethodHandlerAdapter.class)[0]);
+
+	}
+
+	public ModelAndView excuteAction(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		HandlerExecutionChain chain = handlerMapping.getHandler(request);
+
+		request.setAttribute(HandlerMapping.INTROSPECT_TYPE_LEVEL_MAPPING, true);
+
+		final ModelAndView model = handlerAdapter.handle(request, response,
+				chain.getHandler());
+		return model;
+	}
+
+}
---
+++ easynosql/src/test/java/com/sillycat/easynosql/web/UserControllerTest.java
@@ -0,0 +1,85 @@
+package com.sillycat.easynosql.web;
+
+import junit.framework.Assert;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import com.sillycat.easynosql.core.ControllerTestBase;
+import com.sillycat.easynosql.model.User;
+import com.sillycat.easynosql.model.dto.UserListDTO;
+
+public class UserControllerTest extends ControllerTestBase {
+
+	public ObjectMapper jsonMapper;
+
+	@Before
+	public void setUp() {
+		super.setUp();
+		jsonMapper = new ObjectMapper();
+	}
+
+	@Test
+	public void records() throws Exception {
+		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+		MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+		mockRequest.setRequestURI("/users/records");
+		mockRequest.setMethod("GET");
+		this.excuteAction(mockRequest, mockResponse);
+		Assert.assertEquals(mockResponse.getStatus(), 200);
+		UserListDTO result = jsonMapper.readValue(
+				mockResponse.getContentAsString(), UserListDTO.class);
+		Assert.assertNotNull(result.getUsers());
+	}
+
+	@Test
+	public void get() throws Exception {
+		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+		MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+
+		mockRequest.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		mockRequest.setMethod("GET");
+		mockRequest.setRequestURI("/users/get");
+
+		User user = new User();
+		user.setUsername("john");
+		String jsonUser = jsonMapper.writeValueAsString(user);
+		mockRequest.setContent(jsonUser.getBytes());
+
+		this.excuteAction(mockRequest, mockResponse);
+
+		Assert.assertEquals(mockResponse.getStatus(), 200);
+		User result = jsonMapper.readValue(mockResponse.getContentAsString(),
+				User.class);
+		Assert.assertNotNull(result.getUsername());
+		Assert.assertEquals("john", result.getUsername());
+	}
+	
+	@Test
+	public void create() throws Exception {
+		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+		MockHttpServletResponse mockResponse = new MockHttpServletResponse();
+
+		mockRequest.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		mockRequest.setMethod("POST");
+		mockRequest.setRequestURI("/users/create");
+
+		mockRequest.setParameter("username", "test1");
+		mockRequest.setParameter("password", "111111");
+		mockRequest.setParameter("firstName", "test1");
+		mockRequest.setParameter("lastName", "test1");
+		mockRequest.setParameter("role", "1");
+
+		this.excuteAction(mockRequest, mockResponse);
+
+		Assert.assertEquals(mockResponse.getStatus(), 200);
+		User result = jsonMapper.readValue(mockResponse.getContentAsString(),
+				User.class);
+		Assert.assertNotNull(result.getUsername());
+		Assert.assertEquals("test1", result.getUsername());
+	}
+}
