2fed068ff3f1a184bae37ae7afd0ffa9c1d27c19
luohuazju <luohuazju@gmail.com>
2012-06-18 15:19:50 +0800

    add java client example

---
+++ easycometd/src/main/java/com/sillycat/easycometd/comed/chat/client/ConsoleChatClient.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2010 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sillycat.easycometd.comed.chat.client;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.cometd.bayeux.Channel;
+import org.cometd.bayeux.Message;
+import org.cometd.bayeux.client.ClientSessionChannel;
+import org.cometd.client.BayeuxClient;
+import org.cometd.client.transport.LongPollingTransport;
+
+public class ConsoleChatClient {
+
+	private static String nickname = "karl";
+	private static BayeuxClient client;
+	private final ChatListener chatListener = new ChatListener();
+	private final MembersListener membersListener = new MembersListener();
+
+	public static void main(String[] args) throws IOException {
+		ConsoleChatClient client = new ConsoleChatClient();
+		client.go();
+	}
+
+	public void go() {
+		String defaultURL = "http://localhost:8080/easycometd/cometd";
+		String url = defaultURL;
+
+		client = new BayeuxClient(url, LongPollingTransport.create(null));
+		client.getChannel(Channel.META_HANDSHAKE).addListener(
+				new InitializerListener());
+		client.getChannel(Channel.META_CONNECT).addListener(
+				new ConnectionListener());
+
+		client.handshake();
+		boolean success = client.waitFor(1000, BayeuxClient.State.CONNECTED);
+		if (!success) {
+			System.err.printf("Could not handshake with server at %s%n", url);
+			return;
+		}
+
+		String text = "hello";
+
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("user", nickname);
+		data.put("chat", text);
+		client.getChannel("/chat/demo").publish(data);
+
+		client.disconnect(1000);
+	}
+
+	private void initialize() {
+		client.batch(new Runnable() {
+			public void run() {
+				ClientSessionChannel chatChannel = client
+						.getChannel("/chat/demo");
+				chatChannel.unsubscribe(chatListener);
+				chatChannel.subscribe(chatListener);
+
+				ClientSessionChannel membersChannel = client
+						.getChannel("/chat/members");
+				membersChannel.unsubscribe(membersListener);
+				membersChannel.subscribe(membersListener);
+
+				Map<String, Object> data = new HashMap<String, Object>();
+				data.put("user", nickname);
+				data.put("membership", "join");
+				data.put("chat", nickname + " has joined");
+				chatChannel.publish(data);
+			}
+		});
+	}
+
+	private void connectionEstablished() {
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("user", nickname);
+		data.put("room", "/chat/demo");
+		client.getChannel("/service/members").publish(data);
+	}
+
+	private void connectionClosed() {
+		System.err.printf("system: Connection to Server Closed%n");
+	}
+
+	private void connectionBroken() {
+		System.err.printf("system: Connection to Server Broken%n");
+	}
+
+	private class InitializerListener implements
+			ClientSessionChannel.MessageListener {
+		public void onMessage(ClientSessionChannel channel, Message message) {
+			if (message.isSuccessful()) {
+				initialize();
+			}
+		}
+	}
+
+	private class ConnectionListener implements
+			ClientSessionChannel.MessageListener {
+		private boolean wasConnected;
+		private boolean connected;
+
+		public void onMessage(ClientSessionChannel channel, Message message) {
+			if (client.isDisconnected()) {
+				connected = false;
+				connectionClosed();
+				return;
+			}
+
+			wasConnected = connected;
+			connected = message.isSuccessful();
+			if (!wasConnected && connected) {
+				connectionEstablished();
+			} else if (wasConnected && !connected) {
+				connectionBroken();
+			}
+		}
+	}
+
+	private class ChatListener implements ClientSessionChannel.MessageListener {
+		public void onMessage(ClientSessionChannel channel, Message message) {
+			Map<String, Object> data = message.getDataAsMap();
+			String fromUser = (String) data.get("user");
+			String text = (String) data.get("chat");
+			System.out.printf("%s: %s%n", fromUser, text);
+		}
+	}
+
+	private class MembersListener implements
+			ClientSessionChannel.MessageListener {
+		@SuppressWarnings("rawtypes")
+		public void onMessage(ClientSessionChannel channel, Message message) {
+			Object data = message.getData();
+			Object[] members = data instanceof List ? ((List) data).toArray()
+					: (Object[]) data;
+			System.out.printf("Members: %s%n", Arrays.asList(members));
+		}
+	}
+}
--- easycometd/src/main/java/com/sillycat/easycometd/comed/chat/service/ChatService.java
+++ easycometd/src/main/java/com/sillycat/easycometd/comed/chat/service/ChatService.java
@@ -1,23 +1,4 @@
-/*
- * Copyright (c) 2010 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- *
- */
-package com.sillycat.easycometd.comed.chat;
+package com.sillycat.easycometd.comed.chat.service;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -45,9 +26,12 @@ import org.cometd.server.filter.NoMarkupFilter;
 
 @Service("chat")
 public class ChatService {
+	
 	private final ConcurrentMap<String, Map<String, String>> _members = new ConcurrentHashMap<String, Map<String, String>>();
+	
 	@Inject
 	private BayeuxServer _bayeux;
+	
 	@Session
 	private ServerSession _session;
 
@@ -142,12 +126,15 @@ public class ChatService {
 			forward.setData(chat);
 
 			// test for lazy messages
-			if (text.lastIndexOf("lazy") > 0)
+			if (text.lastIndexOf("lazy") > 0){
 				forward.setLazy(true);
+			}
 
-			for (ServerSession peer : peers)
-				if (peer != client)
+			for (ServerSession peer : peers){
+				if (peer != client){
 					peer.deliver(_session, forward);
+				}
+			}
 			client.deliver(_session, forward);
 		}
 	}
