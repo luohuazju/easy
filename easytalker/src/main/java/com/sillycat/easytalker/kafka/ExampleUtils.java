8e26888cd48a95aafe498f7d25c5dbd988b7bab1
luohuazju <luohuazju@gmail.com>
2012-06-27 23:38:52 +0800

    talker message

---
+++ easytalker/src/main/java/com/sillycat/easytalker/kafka/ExampleUtils.java
@@ -0,0 +1,17 @@
+
+package com.sillycat.easytalker.kafka;
+
+import java.nio.ByteBuffer;
+
+import kafka.message.Message;
+
+public class ExampleUtils
+{
+  public static String getMessage(Message message)
+  {
+    ByteBuffer buffer = message.payload();
+    byte [] bytes = new byte[buffer.remaining()];
+    buffer.get(bytes);
+    return new String(bytes);
+  }
+}
