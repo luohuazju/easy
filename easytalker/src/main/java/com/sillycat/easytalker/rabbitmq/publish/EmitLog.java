fc26a01e215fecc2a774f464b2eca0ae53cd58da
luohuazju <luohuazju@gmail.com>
2012-07-03 17:44:24 +0800

    publish and subscribe

---
+++ easytalker/src/main/java/com/sillycat/easytalker/rabbitmq/publish/EmitLog.java
@@ -0,0 +1,30 @@
+package com.sillycat.easytalker.rabbitmq.publish;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLog {
+	
+	private static final String EXCHANGE_NAME = "logs";
+	
+	private final static String SERVER_HOST = "rd.digby.com";
+
+	public static void main(String[] argv) throws Exception {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(SERVER_HOST);
+		
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+		
+		String message = "error debugy inform warning log!";
+		channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		channel.close();
+		connection.close();
+	}
+
+}
---
+++ easytalker/src/main/java/com/sillycat/easytalker/rabbitmq/publish/ReceiveLogs.java
@@ -0,0 +1,36 @@
+package com.sillycat.easytalker.rabbitmq.publish;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class ReceiveLogs {
+	
+	private static final String EXCHANGE_NAME = "logs";
+
+	private final static String SERVER_HOST = "rd.digby.com";
+	
+	public static void main(String[] argv) throws Exception {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(SERVER_HOST);
+		
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+		
+		String queueName = channel.queueDeclare().getQueue();
+		channel.queueBind(queueName, EXCHANGE_NAME, "");
+		
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(queueName, true, consumer);
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
+	}
+}
