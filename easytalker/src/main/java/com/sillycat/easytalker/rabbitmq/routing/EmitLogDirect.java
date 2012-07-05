88c6da1269c70aaa423635e64c0a988ee1d5178d
luohuazju <luohuazju@gmail.com>
2012-07-05 22:55:10 +0800

    routing

---
+++ easytalker/src/main/java/com/sillycat/easytalker/rabbitmq/routing/EmitLogDirect.java
@@ -0,0 +1,31 @@
+package com.sillycat.easytalker.rabbitmq.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class EmitLogDirect {
+	
+	private static final String EXCHANGE_NAME = "direct_logs";
+	
+	private final static String SERVER_HOST = "rd.digby.com";
+
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(SERVER_HOST);
+		
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+		
+		channel.basicPublish(EXCHANGE_NAME, "info", null, "hello info".getBytes());
+		channel.basicPublish(EXCHANGE_NAME, "error", null, "hello error".getBytes());
+		channel.basicPublish(EXCHANGE_NAME, "warning", null, "hello warning".getBytes());
+		channel.basicPublish(EXCHANGE_NAME, "debug", null, "hello debug".getBytes());
+		
+		channel.close();
+		connection.close();
+	}
+
+}
---
+++ easytalker/src/main/java/com/sillycat/easytalker/rabbitmq/routing/ReceiveLogsDirect.java
@@ -0,0 +1,38 @@
+package com.sillycat.easytalker.rabbitmq.routing;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class ReceiveLogsDirect {
+	
+	private static final String EXCHANGE_NAME = "direct_logs";
+	
+	private final static String SERVER_HOST = "rd.digby.com";
+
+	public static void main(String[] argv) throws Exception {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setHost(SERVER_HOST);
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+		
+		channel.exchangeDeclare(EXCHANGE_NAME, "direct");
+		
+		String queueName = channel.queueDeclare().getQueue();
+		
+		channel.queueBind(queueName, EXCHANGE_NAME, "error");
+		channel.queueBind(queueName, EXCHANGE_NAME, "warning");
+		
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(queueName, true, consumer);
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			String routingKey = delivery.getEnvelope().getRoutingKey();
+			System.out.println(" [x] Received '" + routingKey + "':'" + message
+					+ "'");
+		}
+	}
+}
