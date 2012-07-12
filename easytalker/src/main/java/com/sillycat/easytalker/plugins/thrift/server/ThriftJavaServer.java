99a0b6bf3ac6c1bcdab56fb76a8be6cd4a14a870
luohuazju <luohuazju@gmail.com>
2012-07-12 12:52:25 +0800

    thrift server and client side

--- easytalker/src/main/java/com/sillycat/easytalker/plugins/thrift/server/ThriftJavaServer.java
+++ easytalker/src/main/java/com/sillycat/easytalker/plugins/thrift/server/ThriftJavaServer.java
@@ -21,7 +21,7 @@ public class ThriftJavaServer {
 				new BlogServiceImpl());
 		try {
 			TServerTransport serverTransport = new TServerSocket(
-					new InetSocketAddress("0.0.0.0", 981));
+					new InetSocketAddress("0.0.0.0", 9813));
 			Args trArgs = new Args(serverTransport);
 			trArgs.processor(processor);
 			trArgs.protocolFactory(new TBinaryProtocol.Factory(true, true));
