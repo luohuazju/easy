423f9d8a2fc9f807c0dcc424dfe0497406f5b343
carl <4myhappylife@gmail.com>
2012-09-12 12:56:37 -0500

    delete the useless bean

--- easyapi/src/main/java/com/sillycat/easyapi/rest/json/PersonList.java
+++
@@ -1,9 +0,0 @@
-package com.sillycat.easyapi.rest.json;
-
-import java.util.ArrayList;
-
-public class PersonList extends ArrayList<Person>{
-
-	private static final long serialVersionUID = 3423013771933386402L;
-
-}
