add81f3a134c58d28575d051e78f1d6d9403d2db
carl <4myhappylife@gmail.com>
2012-12-06 20:43:51 -0600

    commit R

--- EasyRestClientAndroid/gen/com/sillycat/easyrestclientandroid/R.java
+++ EasyRestClientAndroid/gen/com/sillycat/easyrestclientandroid/R.java
@@ -18,15 +18,22 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int person_name=0x7f070000;
+        public static final int button_json=0x7f070003;
+        public static final int edit_text_id=0x7f070002;
+        public static final int label=0x7f070001;
+        public static final int person_name=0x7f070004;
+        public static final int text_view_description=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int persons_list_item=0x7f030001;
+        public static final int person_get_item=0x7f030001;
+        public static final int persons_list_item=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int base_uri=0x7f040002;
+        public static final int enter_person_id=0x7f040004;
+        public static final int person_get_description=0x7f040003;
         public static final int title_activity_main=0x7f040001;
     }
     public static final class style {
