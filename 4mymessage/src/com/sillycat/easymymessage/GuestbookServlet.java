0db8b8b777cd1bb3a8a9119a8014ac6dcb72a5bd
carl <4myhappylife@gmail.com>
2013-01-06 15:19:55 -0600

    init project

---
+++ 4mymessage/src/com/sillycat/easymymessage/GuestbookServlet.java
@@ -0,0 +1,18 @@
+package com.sillycat.easymymessage;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestbookServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -8974156249244283590L;
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
