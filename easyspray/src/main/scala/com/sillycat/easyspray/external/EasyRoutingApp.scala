0f90b5c66c66ba7ae1539cacd1cfe124a3286958
carl <4myhappylife@gmail.com>
2013-01-08 19:35:29 -0600

    add routing app

---
+++ easyspray/src/main/scala/com/sillycat/easyspray/external/EasyRoutingApp.scala
@@ -0,0 +1,37 @@
+package com.sillycat.easyspray.external
+
+import spray.routing.SimpleRoutingApp
+import spray.http.MediaTypes._
+import scala.concurrent.duration._
+
+object EasyRoutingApp extends App with SimpleRoutingApp {
+
+  startServer(interface = "localhost", port = 8080) {
+    get {
+      path("") {
+        redirect("/hello")
+      } ~
+      path("hello") {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <h1>Say hello to <em>spray</em> on <em>spray-can</em>!</h1>
+              <p>(<a href="/stop?method=post">stop server</a>)</p>
+            </html>
+          }
+        }
+      }
+    } ~
+    (post | parameter('method ! "post")) {
+      path("stop") {
+        complete {
+          system.scheduler.scheduleOnce(1 second span) {
+            system.shutdown()
+          }
+          "Shutting down in 1 second..."
+        }
+      }
+    }
+  }
+  
+}
