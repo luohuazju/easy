70ace87e91b9b6b0cf0e06e098facc5ca1cf544a
carl <4myhappylife@gmail.com>
2013-01-23 17:00:29 -0600

    codes

---
+++ easyakka/src/main/scala/com/sillycat/easyakka/Pi.scala
@@ -0,0 +1,89 @@
+package com.sillycat.easyakka
+
+import akka.actor._
+import akka.routing.RoundRobinRouter
+import akka.util.Duration
+import akka.util.duration._
+
+object Pi extends App {
+
+  calculate(nrOfWorkers = 4, nrOfElements = 10000, nrOfMessages = 10000)
+  calculate(nrOfWorkers = 3, nrOfElements = 10000, nrOfMessages = 10000)
+  
+  sealed trait PiMessage
+  case object Calculate extends PiMessage
+  case class Work(start: Int, nrOfElements: Int) extends PiMessage
+  case class Result(value: Double) extends PiMessage
+  case class PiApproximation(pi: Double, duration: Duration)
+
+  class Worker extends Actor {
+	//get the start and number
+    def calculatePiFor(start: Int, nrOfElements: Int): Double = {
+      var acc = 0.0
+      for (i <- start until (start + nrOfElements))
+        acc += 4.0 * (1 - (i % 2) * 2) / (2 * i + 1)
+      acc   //return the latest line
+    }
+
+    //receive the task data from message, sender back once we are done
+    def receive = {
+      case Work(start, nrOfElements) =>
+        sender ! Result(calculatePiFor(start, nrOfElements)) // perform the work
+    }
+  }
+
+  class Master(nrOfWorkers: Int, 
+    nrOfMessages: Int, 
+    nrOfElements: Int, 
+    listener: ActorRef) extends Actor {  // listener is a ActorRef we can send message
+
+    var pi: Double = _
+    var nrOfResults: Int = _
+    val start: Long = System.currentTimeMillis //get the start time
+
+    //define the number of workers and using round-robin router
+    val workerRouter = context.actorOf(
+      Props[Worker].withRouter(RoundRobinRouter(nrOfWorkers)), name = "workerRouter")
+
+    def receive = {
+      case Calculate =>
+        for (i <- 0 until nrOfMessages) 
+          workerRouter ! Work(i * nrOfElements, nrOfElements) // divide the tasks, send all tasks to workerRouter
+      case Result(value) =>       // case it is the sender message about result from the worker
+        pi += value
+        nrOfResults += 1
+        if (nrOfResults == nrOfMessages) { //get all the messages back
+          listener ! PiApproximation(pi, duration = (System.currentTimeMillis - start).millis)
+          context.stop(self) // stop the master actor
+        }
+    }
+  }
+
+  class Listener extends Actor {  // Everything is Actor
+    def receive = {
+      case PiApproximation(pi, duration) =>
+        println("\n\tPi approximation: \t\t%s\n\tCalculation time: \t%s".format(pi, duration)) 
+        //print out the result
+        context.system.shutdown()
+        //shutdown the system
+    }
+  }
+
+  def calculate(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int) {
+    // Create an Akka system
+    val system = ActorSystem("PiSystem")
+
+    // create the result listener, which will print the result and 
+    // shutdown the system
+    val listener = system.actorOf(Props[Listener], name = "listener")
+
+    // create the master
+    val master = system.actorOf(Props(new Master(
+      nrOfWorkers, nrOfMessages, nrOfElements, listener)),
+      name = "master")
+
+    // start the calculation by send the Calculate message to master actor
+    master ! Calculate
+
+  }
+}
