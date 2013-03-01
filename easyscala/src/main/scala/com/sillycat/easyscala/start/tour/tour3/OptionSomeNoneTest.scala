1a623d77acfc26086dff1293257a978ffabf23d4
carl <4myhappylife@gmail.com>
2013-03-01 16:02:54 -0600

    get the None, Option, Some examples

--- easyscala/src/main/scala/com/sillycat/easyscala/start/tour/tour3/OptionSomeNoneTest.scala
+++ easyscala/src/main/scala/com/sillycat/easyscala/start/tour/tour3/OptionSomeNoneTest.scala
@@ -2,4 +2,28 @@ package com.sillycat.easyscala.start.tour.tour3
 
 object OptionSomeNoneTest {
 
+  def toInt(in: String): Option[Int] = {
+    try {
+      Some(Integer.parseInt(in.trim()))
+    } catch {
+      case e: NumberFormatException => None
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    toInt("100") match {
+      case Some(i) => println(i)
+      case None => println("That didn't work.")
+    } // 100
+    
+    toInt("hello") match {
+      case Some(i) => println(i)
+      case None => println("That didn't work.")
+    } //That didn't work.
+    
+    val bag = List("1", "2", "foo", "3", "bar")
+    val sum = bag.flatMap(toInt).sum  //flatMap know how to handle with None, Option, Some
+    println(sum)  //6
+  }
+
 }
---
+++ easyscala/src/main/scala/com/sillycat/easyscala/start/tour/tour3/Similarity.scala
@@ -0,0 +1,5 @@
+package com.sillycat.easyscala.start.tour.tour3
+
+trait Similarity {
+
+}
