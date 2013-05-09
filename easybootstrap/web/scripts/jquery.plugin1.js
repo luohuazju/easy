1314a7e5726cf6263fa72c91dd7f2177629657d4
carl <4myhappylife@gmail.com>
2013-05-09 14:02:29 -0500

    google maps things

--- easybootstrap/web/scripts/jquery.plugin1.js
+++ easybootstrap/web/scripts/jquery.plugin1.js
@@ -1,3 +1,9 @@
+//require([ "jquery", "jquery.plugin2", "jquery.plugin1" ], function($) {
+
+var t1 = "asdfasdf";
+
 $.fn.sayhello1 = function() {
-    return this.append('<p>Sillycat, go and go!</p>');
+    return this.append('<p>Sillycat, go and go!</p>' + t1);
 };
+
+//});
--- easybootstrap/web/scripts/jquery.plugin2.js
+++ easybootstrap/web/scripts/jquery.plugin2.js
@@ -1,3 +1,7 @@
+//require([ "jquery", "jquery.plugin1", "jquery.plugin2" ], function($) {
+
 $.fn.sayhello2 = function() {
-    return this.append('<p>Kiko, go and go!</p>');
+    return this.append('<p>Kiko, go and go!</p>' + t1);
 };
+
+//});
