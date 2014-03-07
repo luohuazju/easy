b1599b6df57553567334f79708362bf9d24e8f01
Carl <luohuazju@gmail.com>
2014-03-07 14:26:34 -0600

    hello lua launage

--- easylua/src/main/lua/Hello.lua
+++ easylua/src/main/lua/Hello.lua
@@ -1,3 +1,18 @@
 
 print("Hello, Carl")
 
+--function fact(n)
+--    if n== 0 then
+--        return 1
+--    else
+--        return n*fact(n-1)
+--    end
+--end
+--
+--print("enter a number:")
+--a = io.read("*number")
+--print(fact(a))
+
+print(b)
+b = 10
+print(b)
