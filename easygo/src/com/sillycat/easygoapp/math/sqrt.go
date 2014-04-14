a5fbff6243b77b8c640b20f2c128497bfca4662f
Carl <luohuazju@gmail.com>
2014-04-14 14:19:18 -0500

    add some

--- easygo/src/com/sillycat/easygoapp/math/sqrt.go
+++ easygo/src/com/sillycat/easygoapp/math/sqrt.go
@@ -2,8 +2,8 @@ package math
 
 func Sqrt(x float64) float64 {
 	z := 0.0
-    for i := 0; i < 1000; i++ {
-    	z -= (z*z - x) / (2 * x)
-    }
-    return z
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
 }
