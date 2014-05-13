ee8f9731a9e99ec2a7dceecc603255cb0954ec93
Carl <luohuazju@gmail.com>
2014-05-13 09:09:26 -0500

    add php

--- easygo/src/com/sillycat/easygoapp/main.go
+++ easygo/src/com/sillycat/easygoapp/main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"net/http"
 )
 
-type MyMux struct {
+type Server struct {
+	ServerName string
+	ServerIP   string
 }
 
-func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		sayhelloName(w, r)
-		return
-	}
-	http.NotFound(w, r)
-	return
-}
-
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello myroute!")
+type Serverslice struct {
+	Servers []Server
 }
 
 func main() {
-	mux := &MyMux{}
-	http.ListenAndServe(":9090", mux)
+	var s Serverslice
+	s.Servers = append(s.Servers, Server{ServerName: "Shanghai_VPN", ServerIP: "127.0.0.1"})
+	s.Servers = append(s.Servers, Server{ServerName: "Beijing_VPN", ServerIP: "127.0.0.2"})
+	b, err := json.Marshal(s)
+	if err != nil {
+		fmt.Println("json err:", err)
+	}
+	fmt.Println(string(b))
 }
