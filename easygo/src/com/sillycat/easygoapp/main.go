ce59ac34b59c5659dc667bc17a8e4034ea925852
Carl <luohuazju@gmail.com>
2014-05-21 08:53:01 -0500

    improve the json handle in go routers

--- easygo/src/com/sillycat/easygoapp/main.go
+++ easygo/src/com/sillycat/easygoapp/main.go
@@ -15,80 +15,71 @@ type Bug struct {
 	BugDesn   string
 }
 
-type BugResult struct {
-	Bugs   []Bug
-	Status string
+type JSONResult map[string]interface{}
+
+func (r JSONResult) String() (s string) {
+	b, err := json.Marshal(r)
+	if err != nil {
+		s = "json err: " + err.Error()
+		return
+	}
+	s = string(b)
+	return
+}
+
+func ParseJSON(r *http.Request) Bug {
+	var b Bug
+	c, _ := ioutil.ReadAll(r.Body)
+	fmt.Println("ParseJSON called with Body=" + string(c))
+
+	json.Unmarshal(c, &b)
+	return b
 }
 
 func getBug(w http.ResponseWriter, r *http.Request) {
 	b1 := Bug{Id: "1", BugNumber: "bug1", BugName: "bug1", BugDesn: "desn1"}
-	re := BugResult{Status: "Ok", Bugs: []Bug{b1}}
-	b, err := json.Marshal(re)
-	if err != nil {
-		fmt.Fprint(w, "json err: %s", err)
-	}
-	fmt.Fprint(w, string(b))
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JSONResult{"status": "ok", "bugs": []Bug{b1}})
 }
 
 func updateBug(w http.ResponseWriter, r *http.Request) {
-	var b Bug
-	c, err := ioutil.ReadAll(r.Body)
-	fmt.Println("updateBug called with Body=" + string(c))
-
-	json.Unmarshal(c, &b)
+	b := ParseJSON(r)
 
 	fmt.Println("updateBug called with Id=" + b.Id)
 	fmt.Println("updateBug called with BugNumber=" + b.BugNumber)
 	fmt.Println("updateBug called with BugName=" + b.BugName)
 	fmt.Println("updateBug called with BugDesn=" + b.BugDesn)
 
-	re := BugResult{Status: "OK", Bugs: []Bug{b}}
-	re_json, err := json.Marshal(re)
-	if err != nil {
-		fmt.Fprint(w, "json err: %s", err)
-	}
-	fmt.Fprint(w, string(re_json))
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JSONResult{"status": "ok", "bugs": []Bug{b}})
 }
 
 func deleteBug(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["Id"]
 	fmt.Println("deleteBug called with Id = ", id)
 
-	re := BugResult{Status: "OK"}
-	b, err := json.Marshal(re)
-	if err != nil {
-		fmt.Fprint(w, "json err: %s", err)
-	}
-	fmt.Fprint(w, string(b))
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JSONResult{"status": "ok"})
 }
 
 func addBug(w http.ResponseWriter, r *http.Request) {
-	var b Bug
-	content, err := ioutil.ReadAll(r.Body)
-	fmt.Println("addBug called with Body=" + string(content))
+	b := ParseJSON(r)
 
-	json.Unmarshal(content, &b)
 	fmt.Println("addBug called with BugNumber=" + b.BugNumber)
 	fmt.Println("addBug called with BugName=" + b.BugName)
 	fmt.Println("addBug called with BugDesn=" + b.BugDesn)
 
-	re := BugResult{Status: "OK", Bugs: []Bug{b}}
-	re_json, err := json.Marshal(re)
-	if err != nil {
-		fmt.Fprint(w, "json err: %s", err)
-	}
-	fmt.Fprint(w, string(re_json))
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JSONResult{"status": "ok", "bugs": []Bug{b}})
 }
 
 func listBug(w http.ResponseWriter, r *http.Request) {
 	b1 := Bug{Id: "1", BugNumber: "bug1", BugName: "bug1", BugDesn: "desn1"}
 	b2 := Bug{Id: "2", BugNumber: "bug2", BugName: "bug2", BugDesn: "desn2"}
-	re := BugResult{Status: "Ok", Bugs: []Bug{b1, b2}}
-	b, err := json.Marshal(re)
-	if err != nil {
-		fmt.Fprint(w, "json err: %s", err)
-	}
-	fmt.Fprint(w, string(b))
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, JSONResult{"status": "ok", "bugs": []Bug{b1, b2}})
 }
 
 func main() {
