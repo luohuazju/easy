60e3feb3101ace0c9d38d0246a7dd911cd2a0ee0
luohuazju <luohuazju@gmail.com>
2015-05-17 23:35:44 -0500

    algorithm

---
+++ easyalgorithm/src/main/java/com/sillycat/easyalgorithm/sort/Arraysort.java
@@ -0,0 +1,29 @@
+package com.sillycat.easyalgorithm.sort;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+public class Arraysort implements Sorter {
+	
+	@Override
+	public void sort(List<Integer> arraylist) {
+		Collections.sort(arraylist);
+	}
+
+	public static void main(String[] args) {
+		Vector<Integer> v1 = new Vector<Integer>();
+		v1.add(1);
+		v1.add(9);
+		v1.add(3);
+		v1.add(11);
+		v1.add(6);
+		System.out.println(v1);
+		Arraysort sort1 = new Arraysort();
+		sort1.sort(v1);
+		System.out.println(v1);
+	}
+
+
+
+}
