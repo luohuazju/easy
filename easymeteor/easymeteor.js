d8e447066e576d96cd0a64a6d51902c446e49632
Carl Luo <cluo@jobs2careers.com>
2015-06-24 17:07:19 -0500

    init the project easymeteor

---
+++ easymeteor/easymeteor.js
@@ -0,0 +1,47 @@
+// simple-todos.js
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find();
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
+  // In the client code, below everything else
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
